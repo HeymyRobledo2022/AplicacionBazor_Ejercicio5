e5aa6985cf590aa1e185d71d61f50ecb3347e27a
HeymyRobledo2022 <116927220+HeymyRobledo2022@users.noreply.github.com>
2022-11-19 01:19:44 -0600

    Codigo

--- AplicacionWebRobledoHeymy/Blazor/Program.cs
+++ AplicacionWebRobledoHeymy/Blazor/Program.cs
@@ -1,27 +1,47 @@
 
+using Blazor;
+using Blazor.Interfaces;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Error");
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+
+        Config cadenaConexion = new Config(builder.Configuration.GetConnectionString("MySQL"));
+        builder.Services.AddSingleton(cadenaConexion);
+
+        builder.Services.AddScoped<ILoginServicio, ILoginServicio>();
+
+
+        using (var app = builder.Build())
+        {
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
 }
-
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
--- AplicacionWebRobledoHeymy/Modelos/Login.cs
+++ AplicacionWebRobledoHeymy/Modelos/Login.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Modelos
+﻿namespace Modelos
 {
     public class Login
     {
-
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
     }
 }
